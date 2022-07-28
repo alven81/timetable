d6abb1ffc5d5d5f67dddb5d01eaa2189f43ca5ce
Aliaksandr Kot <alven81.ak@gmail.com>
2022-07-28 08:36:35 +0200

    finished with desktop first

--- src/Tab/Tab.jsx
+++ src/Tab/Tab.jsx
@@ -16,34 +16,23 @@ const Tab = ({ tableData }) => {
     tableData.sort(sortByField(sortBy));
 
     function timeConverter(time) {
-        let date = Date.parse(time)/1000
+        let date = Date.parse(time)/1000;
         let nextDate = new Date(date * 1000);
         return nextDate.getHours() + ":" + nextDate.getMinutes()
     }
+
+    // function setMaxLengthDest(data) {
+    //     if (data.split("").length < 19) {
+    //     let tempText = data.split("").concat(Array(20-data.split("").length))
+    //        return tempText.join("");
+    //     };
+    // }
 
     return (
         <>
             <ul>
                 <li>
-                    <div>
-                        <button
-                            onClick={() => {
-                                setSortBy("apname");
-                            }}
-                        >
-                            DESTINATION
-                        </button>
-                    </div>
-                    <div>
-                        <button
-                            onClick={() => {
-                                setSortBy("fnr");
-                            }}
-                        >
-                            FLIGHT
-                        </button>
-                    </div>
-                    <div>
+                <div className='main-table_info_time'>
                         <button
                             onClick={() => {
                                 setSortBy("sched");
@@ -52,16 +41,25 @@ const Tab = ({ tableData }) => {
                             TIME
                         </button>
                     </div>
-                    <div>
+                    <div className='main-table_info_destination'>
                         <button
                             onClick={() => {
-                                setSortBy("status");
+                                setSortBy("apname");
                             }}
                         >
-                            REMARKS
+                            DESTINATION
                         </button>
                     </div>
-                    <div>
+                    <div className='main-table_info_fnr'>
+                        <button
+                            onClick={() => {
+                                setSortBy("fnr");
+                            }}
+                        >
+                            FLIGHT
+                        </button>
+                    </div>
+                    <div className='main-table_info_gate'>
                         <button
                             onClick={() => {
                                 setSortBy("terminal");
@@ -70,6 +68,16 @@ const Tab = ({ tableData }) => {
                             GATE
                         </button>
                     </div>
+                    <div className='main-table_info_status'>
+                        <button
+                            onClick={() => {
+                                setSortBy("status");
+                            }}
+                        >
+                            REMARKS
+                        </button>
+                    </div>
+
                 </li>
                 {
                     !timeTable ? (
@@ -77,21 +85,22 @@ const Tab = ({ tableData }) => {
                     ) : (
                         timeTable.map((item) => (
                             <li key={item.id}>
-                                <div>
-                                    <p>{item.apname}</p>
-                                </div>
-                                <div>
-                                    <p>{item.fnr}</p>
-                                </div>
-                                <div>
+                                                                <div className='main-table_info_time'>
                                     <p>{timeConverter(item.sched)}</p>
                                 </div>
-                                <div>
-                                    <p>{item.status}</p>
+                                <div className='main-table_info_destination'>
+                                    <p>{item.apname}</p>
                                 </div>
-                                <div>
+                                <div className='main-table_info_fnr'>
+                                    <p>{item.fnr}</p>
+                                </div>
+                                <div className='main-table_info_gate'>
                                     <p>{item.terminal}</p>
                                 </div>
+                                <div className='main-table_info_status'>
+                                    <p>{item.status}</p>
+                                </div>
+
                             </li>
                         ))
                     )
